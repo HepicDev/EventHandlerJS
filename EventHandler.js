a7eb7d1f4651451e9d2f815b011e75ea9c832c9d
Misa Stefanovic <40996107+MikMikYT@users.noreply.github.com>
2021-12-09 01:26:27 +0100

    add listenerCount and listeners methods, add missing semicolons

--- EventHandler.js
+++ EventHandler.js
@@ -18,13 +18,13 @@ class EventHandler {
 
     constructor() {
         this.#events = {};
-        this.initAliases()
+        this.initAliases();
     }
 
     initAliases() {
         this.addListener = this.on;
-        this.removeListener = this.off
-        this.removeAllListeners = this.offAll
+        this.removeListener = this.off;
+        this.removeAllListeners = this.offAll;
     }
 
     ensureEventArrays(name) {
@@ -32,55 +32,65 @@ class EventHandler {
     }
 
     on(name, listener) {
-        this.emit("newListener", name, listener)
+        this.emit("newListener", name, listener);
         this.ensureEventArrays(name);
         this.#events[name].on.push(listener);
-        return this
+        return this;
     }
 
     
     once(name, listener) {
-        this.emit("newListener", name, listener)
+        this.emit("newListener", name, listener);
         this.ensureEventArrays(name);
         this.#events[name].once.push(listener);
-        return this
+        return this;
     }
 
     off(name, listener) {
-        this.ensureEventArrays(name)
+        this.ensureEventArrays(name);
         var removed = this.#events[name].once.remove(listener, true);
         if (!removed) removed = this.#events[name].on.remove(listener, true);
-        if (removed) this.emit("removeListener", name, listener)
-        return this
+        if (removed) this.emit("removeListener", name, listener);
+        return this;
     }
 
     
     offAll(name) {
-        this.#events[name].on = []
-        this.#events[name].once = []
-        return this
+        this.#events[name].on = [];
+        this.#events[name].once = [];
+        return this;
     }
 
 
     emit(name, ...args) {
-        var success = false
+        var success = false;
         this.ensureEventArrays(name);
         if (this.#events[name].once.length > 0) {
             this.#events[name].once.forEach(listener => listener(...args));
-            success = true
+            success = true;
         }
 
         this.#events[name].once = [];
         if (this.#events[name].on.length > 0) {
             this.#events[name].on.forEach(listener => listener(...args));
-            success = true
+            success = true;
         }
-        return success
+        return success;
     }
 
     eventNames() {
         return Object.keys(this.#events);
     }
+
+    listenerCount() {
+        return this.eventNames().length;
+    }
+
+    listeners(name) {
+        return this.#events[name];
+    }
+
+    
 
 }
 
